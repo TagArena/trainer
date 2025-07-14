62e568da68b52e04ba29a509ba3bb727c609114b
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2025-07-14 15:03:39 +0200

    Add files via upload

--- src/main/java/com/tagarena/trainer/service/model/exception/TrainerNotFoundException.java
+++ src/main/java/com/tagarena/trainer/service/model/exception/TrainerNotFoundException.java
@@ -1,5 +1,9 @@
 package com.tagarena.trainer.service.model.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import org.springframework.http.HttpStatus;
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class TrainerNotFoundException extends RuntimeException {
 
