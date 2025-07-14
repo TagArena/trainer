a754f8c2ef85dd93236a39ddb9e1953eca3a0221
roboter5123 <pascal.roboter@hotmail.com>
2025-07-14 18:02:49 +0200

    Revert "Add files via upload"
    This reverts commit 5c24fb679eab9396f7e3449b66cda6651e0c639a.

--- src/main/java/com/tagarena/trainer/repository/model/TrainerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tagarena.trainer.repository.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainerRepository extends JpaRepository<TrainerEntity, Long> {
-
-}
--- src/main/java/com/tagarena/trainer/service/model/exception/TrainerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.tagarena.trainer.service.model.exception;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class TrainerNotFoundException extends RuntimeException {
-
-	public TrainerNotFoundException(String message) {
-		super(message);
-	}
-}
